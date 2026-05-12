2bec3d23035d0a8dc3af9c3fdb5f5956d031cb55
Karthik Vadlapatla <vadlapatla.karthik@gmail.com>
2026-05-13 05:22:10 +0530

    feat: add ViewModeToggle component

--- components/panels/index.ts
+++ components/panels/index.ts
@@ -2,3 +2,4 @@ export { TopBar } from './TopBar';
 export { LeftPanel } from './LeftPanel';
 export { ResizeHandle } from './ResizeHandle';
 export { DefaultsTable } from './DefaultsTable';
+export { ViewModeToggle } from './ViewModeToggle';
